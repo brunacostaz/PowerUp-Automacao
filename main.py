3c892540fdfd769b83c04fb77d23a8de41693f2a
brunacostaz <brunacstcandeias@gmail.com>
2024-07-09 00:59:46 -0300

    first commit - automação de tarefas com python

---
+++ main.py
@@ -0,0 +1,80 @@
+# passo 1: abrir o chrome e acessar o site (https://dlp.hashtagtreinamentos.com/python/intensivao/tabela)
+# passo 2: fazer login com email e senha
+# passo 3: abrir o banco de dados com os produtos
+# passo 4: registrar os produtos na base de dados
+# passo 5: repetir o passo 4 até ter cadastrado todos os produtos
+
+#bibliotecas necessárias: pyautogui | time | pandas (openpyxl numpy)
+
+import pyautogui 
+import time 
+import pandas
+
+pyautogui.PAUSE = 1
+
+#passo 1 - abrir o windows, acessar o chrome e entrar no site da empresa
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.press('enter')
+
+time.sleep(3)
+
+#passo 2 - fazer o cadastro  
+#clicar no campo de email
+pyautogui.click(x=550, y=370)
+
+#preencher email
+pyautogui.hotkey('ctrl','a')
+pyautogui.write('automacaopy@gmail.com')
+
+#passar e preencher o campo de senha
+pyautogui.press('tab')
+pyautogui.write('minhaprimeiraautomacao')
+
+#clicar no botão para entrar no cadastro
+pyautogui.click(x=670, y=527)
+
+time.sleep(3)
+
+#Passo 3 - acessar a base de dados com os produtos
+
+base_dados = pandas.read_csv('produtos.csv')
+
+#Passo 4 - registrar os produtos 
+for linha in base_dados.index:
+    #diminuir o tempo de pausa para cadastrar mais rápido       
+
+    pyautogui.PAUSE = 0.5
+
+    #clicar no primeiro campo para escrever o código do produto
+    pyautogui.click(x=479, y=259)
+
+    #base_dados.loc[linha,coluna] - localiza o item pelo indice da linha e o nome da coluna
+    pyautogui.write(str(base_dados.loc[linha,'codigo']))
+    pyautogui.press('tab')
+
+    pyautogui.write(str(base_dados.loc[linha,'marca']))
+    pyautogui.press('tab')
+ 
+    pyautogui.write(str(base_dados.loc[linha,'tipo']))
+    pyautogui.press('tab')
+
+    pyautogui.write(str(base_dados.loc[linha,'categoria']))
+    pyautogui.press('tab')
+
+    pyautogui.write(str(base_dados.loc[linha,'preco_unitario']))
+    pyautogui.press('tab')
+
+    pyautogui.write(str(base_dados.loc[linha,'custo']))
+    pyautogui.press('tab')
+
+    obs = (str(base_dados.loc[linha,'obs']))
+    if obs != 'nan': #se esse campo estiver vazio ele não irá preencher
+        pyautogui.write(obs)
+    pyautogui.press('tab')
+
+    pyautogui.press('enter')
+    pyautogui.scroll(1000)
+
